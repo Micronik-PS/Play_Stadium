d72c8b85137a3f6527ceaa7b8dff817d9369036d
Micronik-PS <micronik.ps@outlook.com>
2025-11-04 12:20:39 +0300

    Создан PlayStadiumGameInstance

---
+++ Source/Play_Stadium/Core/PlayStadiumGameInstance.cpp
@@ -0,0 +1,1 @@
+#include "PlayStadiumGameInstance.h"
---
+++ Source/Play_Stadium/Core/PlayStadiumGameInstance.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "PlayStadiumGameInstance.generated.h"
+
+
+UCLASS()
+class PLAY_STADIUM_API UPlayStadiumGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};

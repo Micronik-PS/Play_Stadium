cf0fec7da0f50f8b1058de644ab633c2c385f1b6
Micronik-PS <micronik.ps@outlook.com>
2025-11-12 01:12:02 +0300

    Создана предустановка для стартового уровня

---
+++ Source/Play_Stadium/Core/GameModes/StartGameMode/StartGameMode.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "StartGameMode.generated.h"
+
+
+UCLASS()
+class PLAY_STADIUM_API AStartGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};

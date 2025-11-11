cb13bb04e1f27009be16f23d053f8b20226c5d82
Micronik-PS <micronik.ps@outlook.com>
2025-11-11 22:14:42 +0300

    Создан виджет стартового окна

---
+++ Source/Play_Stadium/Core/UI/StartMenuWidget.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "StartMenuWidget.generated.h"
+
+
+UCLASS()
+class PLAY_STADIUM_API UStartMenuWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};

af63236c1be6b0c9175067ee4987180fccac62a4
Micronik-PS <micronik.ps@outlook.com>
2025-11-12 18:48:00 +0300

    Создан стартовый PlayerController

---
+++ Source/Play_Stadium/Core/PlayerControllers/StartPlayerController/StartPlayerController.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "StartPlayerController.generated.h"
+
+
+UCLASS()
+class PLAY_STADIUM_API AStartPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};

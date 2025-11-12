93f27d1f8ab0551c1dc6d4f402ac3172718db74a
Micronik-PS <64549940+Micronik-PS@users.noreply.github.com>
2025-11-12 20:31:24 +0300

    Open start menu widget on controller begin play

--- Source/Play_Stadium/Core/PlayerControllers/StartPlayerController/StartPlayerController.cpp
+++ Source/Play_Stadium/Core/PlayerControllers/StartPlayerController/StartPlayerController.cpp
@@ -1,1 +1,25 @@
 #include "StartPlayerController.h"
+
+#include "Blueprint/UserWidget.h"
+#include "Core/UI/StartMenuWidget.h"
+
+void AStartPlayerController::BeginPlay()
+{
+        Super::BeginPlay();
+
+        if (!IsLocalController())
+        {
+                return;
+        }
+
+        if (!StartMenuWidgetClass)
+        {
+                return;
+        }
+
+        StartMenuWidgetInstance = CreateWidget<UStartMenuWidget>(this, StartMenuWidgetClass);
+        if (StartMenuWidgetInstance)
+        {
+                StartMenuWidgetInstance->AddToViewport();
+        }
+}

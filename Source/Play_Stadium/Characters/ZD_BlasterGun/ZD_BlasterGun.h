b77d7e58151701ea2a1f0c01dd9ce0487646a5af
Micronik-PS <64549940+Micronik-PS@users.noreply.github.com>
2025-11-25 00:39:25 +0300

    Настроено передвижение персонажа AZD_BlasterGun

--- Source/Play_Stadium/Characters/ZD_BlasterGun/ZD_BlasterGun.h
+++ Source/Play_Stadium/Characters/ZD_BlasterGun/ZD_BlasterGun.h
@@ -1,13 +1,26 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "InputActionValue.h"
 #include "PaperZDCharacter.h"
 #include "ZD_BlasterGun.generated.h"
+
+class UInputAction;
 
 
 UCLASS()
 class PLAY_STADIUM_API AZD_BlasterGun : public APaperZDCharacter
 {
 	GENERATED_BODY()
-	
+
+	public:
+	virtual void SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) override;
+
+	private:
+	void MoveHorizontal(const FInputActionValue& Value);
+
+	private:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input", meta = (AllowPrivateAccess = "true"))
+	UInputAction* MoveAction = nullptr;
+
 };

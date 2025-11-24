69e1859c1759ec7d0d9711b2227d359fcac15168
Micronik-PS <64549940+Micronik-PS@users.noreply.github.com>
2025-11-25 01:08:09 +0300

    APlayStadiumLevelBase при старте вселяет игрока в выбранный CameraActor

--- Source/Play_Stadium/Maps/PlayStadiumLevelBase.h
+++ Source/Play_Stadium/Maps/PlayStadiumLevelBase.h
@@ -4,10 +4,17 @@
 #include "Engine/LevelScriptActor.h"
 #include "PlayStadiumLevelBase.generated.h"
 
+class ACameraActor;
 
 UCLASS(Abstract)
 class PLAY_STADIUM_API APlayStadiumLevelBase : public ALevelScriptActor
 {
 	GENERATED_BODY()
-	
+
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	UPROPERTY(EditAnywhere, Category = "Camera")
+	ACameraActor* LevelCameraActor = nullptr;
 };

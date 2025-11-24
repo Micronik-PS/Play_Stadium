7ce9ad63ca1685d81a5a85cf90bc469860dfebac
Micronik-PS <micronik.ps@outlook.com>
2025-11-25 00:57:42 +0300

    Добавлен базовый C++ класс для всех игровых карт

---
+++ Source/Play_Stadium/Maps/PlayStadiumLevelBase.cpp
@@ -0,0 +1,1 @@
+#include "PlayStadiumLevelBase.h"
---
+++ Source/Play_Stadium/Maps/PlayStadiumLevelBase.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/LevelScriptActor.h"
+#include "PlayStadiumLevelBase.generated.h"
+
+
+UCLASS(Abstract)
+class PLAY_STADIUM_API APlayStadiumLevelBase : public ALevelScriptActor
+{
+	GENERATED_BODY()
+	
+};

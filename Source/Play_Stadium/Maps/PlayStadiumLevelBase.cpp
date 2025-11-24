d59f979e67325be001ffa648d3a6a1bf8219e9ef
Micronik-PS <64549940+Micronik-PS@users.noreply.github.com>
2025-11-25 01:02:13 +0300

    Set level camera as player view on load

--- Source/Play_Stadium/Maps/PlayStadiumLevelBase.cpp
+++ Source/Play_Stadium/Maps/PlayStadiumLevelBase.cpp
@@ -1,1 +1,16 @@
 #include "PlayStadiumLevelBase.h"
+
+#include "Camera/CameraActor.h"
+#include "GameFramework/PlayerController.h"
+
+void APlayStadiumLevelBase::BeginPlay()
+{
+	Super::BeginPlay();
+
+	APlayerController* PlayerController = GetWorld() ? GetWorld()->GetFirstPlayerController() : nullptr;
+	if (PlayerController && LevelCameraActor)
+	{
+		FViewTargetTransitionParams TransitionParams;
+		PlayerController->SetViewTarget(LevelCameraActor, TransitionParams);
+	}
+}
--- Source/Play_Stadium/Maps/PlayStadiumLevelBase.h
+++ Source/Play_Stadium/Maps/PlayStadiumLevelBase.h
@@ -2,12 +2,21 @@
 
 #include "CoreMinimal.h"
 #include "Engine/LevelScriptActor.h"
-#include "PlayStadiumLevelBase.generated.h"
 
+class ACameraActor;
+
+#include "PlayStadiumLevelBase.generated.h"
 
 UCLASS(Abstract)
 class PLAY_STADIUM_API APlayStadiumLevelBase : public ALevelScriptActor
 {
 	GENERATED_BODY()
-	
+
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	/** Camera to set as the player's initial view target when the level loads. */
+	UPROPERTY(EditAnywhere, Category = "Camera")
+	ACameraActor* LevelCameraActor = nullptr;
 };

4816974ac1d59a2a6e43f65ad2b06bd507db7ef9
Micronik-PS <64549940+Micronik-PS@users.noreply.github.com>
2025-11-25 00:30:52 +0300

    Add horizontal movement input

--- Source/Play_Stadium/Characters/ZD_BlasterGun/ZD_BlasterGun.cpp
+++ Source/Play_Stadium/Characters/ZD_BlasterGun/ZD_BlasterGun.cpp
@@ -1,1 +1,27 @@
 #include "ZD_BlasterGun.h"
+
+#include "EnhancedInputComponent.h"
+
+void AZD_BlasterGun::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+    Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+    if (UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(PlayerInputComponent))
+    {
+        if (MoveAction)
+        {
+            EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &AZD_BlasterGun::MoveHorizontal);
+        }
+    }
+}
+
+void AZD_BlasterGun::MoveHorizontal(const FInputActionValue& Value)
+{
+    const float Direction = Value.Get<float>();
+    if (FMath::IsNearlyZero(Direction))
+    {
+        return;
+    }
+
+    AddMovementInput(FVector::ForwardVector, Direction);
+}

496ed35c7e14cd9280166e4ac2eaf8884652b46a
Micronik-PS <micronik.ps@outlook.com>
2025-11-26 01:08:53 +0300

    Созданы классы для снаряда персонажа BlasterGun

---
+++ Source/Play_Stadium/Projectiles/BlasterGunProjectile/BlasterGunProjectile.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "BlasterGunProjectile.generated.h"
+
+UCLASS()
+class PLAY_STADIUM_API ABlasterGunProjectile : public AActor
+{
+	GENERATED_BODY()
+	
+
+};

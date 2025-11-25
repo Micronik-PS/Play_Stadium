4ac3a2f90bbcf83fe2d718f8a8fed22a58057fe5
Micronik-PS <64549940+Micronik-PS@users.noreply.github.com>
2025-11-26 00:57:30 +0300

    Добавлена логика стрельбы для BlasterGun

--- Source/Play_Stadium/Characters/ZD_BlasterGun/ZD_BlasterGun.h
+++ Source/Play_Stadium/Characters/ZD_BlasterGun/ZD_BlasterGun.h
@@ -6,21 +6,32 @@
 #include "ZD_BlasterGun.generated.h"
 
 class UInputAction;
+class AActor;
 
 
 UCLASS()
 class PLAY_STADIUM_API AZD_BlasterGun : public APaperZDCharacter
 {
-	GENERATED_BODY()
+        GENERATED_BODY()
 
-	public:
-	virtual void SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) override;
+        public:
+        virtual void SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) override;
 
-	private:
-	void MoveHorizontal(const FInputActionValue& Value);
+        private:
+        void MoveHorizontal(const FInputActionValue& Value);
+        void Fire();
 
-	private:
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input", meta = (AllowPrivateAccess = "true"))
-	UInputAction* MoveAction = nullptr;
+        private:
+        UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input", meta = (AllowPrivateAccess = "true"))
+        UInputAction* MoveAction = nullptr;
+
+        UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input", meta = (AllowPrivateAccess = "true"))
+        UInputAction* FireAction = nullptr;
+
+        UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Combat", meta = (AllowPrivateAccess = "true"))
+        TSubclassOf<AActor> ProjectileClass;
+
+        UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Combat", meta = (AllowPrivateAccess = "true"))
+        FVector MuzzleOffset = FVector(50.0f, 0.0f, 0.0f);
 
 };

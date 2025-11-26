ceba455e2e5235d586ee8545b5d0e5f2a3c77ece
Micronik-PS <64549940+Micronik-PS@users.noreply.github.com>
2025-11-27 01:11:12 +0300

    Реализация полета снаряда

--- Source/Play_Stadium/Projectiles/BlasterGunProjectile/BlasterGunProjectile.h
+++ Source/Play_Stadium/Projectiles/BlasterGunProjectile/BlasterGunProjectile.h
@@ -1,13 +1,35 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Components/SceneComponent.h"
 #include "GameFramework/Actor.h"
+#include "PaperSpriteComponent.h"
 #include "BlasterGunProjectile.generated.h"
+
+class UProjectileMovementComponent;
+class UPaperSprite;
 
 UCLASS()
 class PLAY_STADIUM_API ABlasterGunProjectile : public AActor
 {
 	GENERATED_BODY()
-	
 
+public:
+	ABlasterGunProjectile();
+
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
+	USceneComponent* Root = nullptr;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
+	UPaperSpriteComponent* ProjectileSpriteComponent = nullptr;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Movement", meta = (AllowPrivateAccess = "true"))
+	UProjectileMovementComponent* ProjectileMovementComponent = nullptr;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Visual", meta = (AllowPrivateAccess = "true"))
+	UPaperSprite* ProjectileSprite = nullptr;
 };

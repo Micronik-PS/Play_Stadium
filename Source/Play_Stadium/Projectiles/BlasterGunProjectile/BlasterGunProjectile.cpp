4891aeeef00230e2af7e6080a5300dbf2f5b17ce
Micronik-PS <64549940+Micronik-PS@users.noreply.github.com>
2025-11-27 01:09:36 +0300

    Add collision handling to blaster projectile

--- Source/Play_Stadium/Projectiles/BlasterGunProjectile/BlasterGunProjectile.cpp
+++ Source/Play_Stadium/Projectiles/BlasterGunProjectile/BlasterGunProjectile.cpp
@@ -1,1 +1,51 @@
 #include "BlasterGunProjectile.h"
+
+#include "GameFramework/ProjectileMovementComponent.h"
+
+ABlasterGunProjectile::ABlasterGunProjectile()
+{
+    PrimaryActorTick.bCanEverTick = false;
+
+    CollisionComponent = CreateDefaultSubobject<UBoxComponent>(TEXT("Collision"));
+    CollisionComponent->SetBoxExtent(FVector(8.0f, 8.0f, 8.0f));
+    CollisionComponent->SetCollisionProfileName(TEXT("Projectile"));
+    CollisionComponent->SetNotifyRigidBodyCollision(true);
+    CollisionComponent->SetGenerateOverlapEvents(true);
+    SetRootComponent(CollisionComponent);
+    CollisionComponent->OnComponentHit.AddDynamic(this, &ABlasterGunProjectile::OnProjectileHit);
+
+    ProjectileSpriteComponent = CreateDefaultSubobject<UPaperSpriteComponent>(TEXT("Sprite"));
+    ProjectileSpriteComponent->SetupAttachment(RootComponent);
+
+    ProjectileMovementComponent = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("ProjectileMovement"));
+    ProjectileMovementComponent->InitialSpeed = 1500.0f;
+    ProjectileMovementComponent->MaxSpeed = 1500.0f;
+    ProjectileMovementComponent->ProjectileGravityScale = 0.0f;
+    ProjectileMovementComponent->bRotationFollowsVelocity = true;
+}
+
+void ABlasterGunProjectile::BeginPlay()
+{
+    Super::BeginPlay();
+
+    if (ProjectileSprite && ProjectileSpriteComponent)
+    {
+        ProjectileSpriteComponent->SetSprite(ProjectileSprite);
+    }
+
+    if (ProjectileMovementComponent)
+    {
+        const FVector Direction = GetActorForwardVector();
+        ProjectileMovementComponent->Velocity = Direction * ProjectileMovementComponent->InitialSpeed;
+    }
+}
+
+void ABlasterGunProjectile::OnProjectileHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
+{
+    if (!OtherActor || OtherActor == this || !OtherComp)
+    {
+        return;
+    }
+
+    Destroy();
+}
--- Source/Play_Stadium/Projectiles/BlasterGunProjectile/BlasterGunProjectile.h
+++ Source/Play_Stadium/Projectiles/BlasterGunProjectile/BlasterGunProjectile.h
@@ -1,13 +1,40 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Components/BoxComponent.h"
+#include "Components/SceneComponent.h"
 #include "GameFramework/Actor.h"
+#include "PaperSpriteComponent.h"
 #include "BlasterGunProjectile.generated.h"
+
+class UProjectileMovementComponent;
+class UPaperSprite;
+class UPrimitiveComponent;
 
 UCLASS()
 class PLAY_STADIUM_API ABlasterGunProjectile : public AActor
 {
-	GENERATED_BODY()
-	
+    GENERATED_BODY()
 
+public:
+    ABlasterGunProjectile();
+
+protected:
+    virtual void BeginPlay() override;
+
+private:
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
+    UBoxComponent* CollisionComponent = nullptr;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
+    UPaperSpriteComponent* ProjectileSpriteComponent = nullptr;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Movement", meta = (AllowPrivateAccess = "true"))
+    UProjectileMovementComponent* ProjectileMovementComponent = nullptr;
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Visual", meta = (AllowPrivateAccess = "true"))
+    UPaperSprite* ProjectileSprite = nullptr;
+
+    UFUNCTION()
+    void OnProjectileHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
 };
